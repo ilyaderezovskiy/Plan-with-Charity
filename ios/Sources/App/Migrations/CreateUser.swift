f48f6db901e615892ff0f3bfbd2dc77d60bd3c81
ilyaderezovskiy <71149562+ilyaderezovskiy@users.noreply.github.com>
2023-04-13 03:01:02 +0300

    iOS app backend
    iOS app backend

---
+++ ios/Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,28 @@
+//
+//  CreateUser.swift
+//  
+//
+//  Created by Ilya Derezovskiy on 02.04.2023.
+//
+
+import Fluent
+import Vapor
+
+struct CreateUser: AsyncMigration {
+    
+    // Создание базы данных
+    func prepare(on database: Database) async throws {
+        try await database.schema("users")
+            .id()
+            .field("username", .string, .required)
+            .unique(on: "username")
+            .field("email", .string, .required)
+            .field("password_hash", .string, .required)
+            .field("sum", .int, .required)
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema("users").delete()
+    }
+}
